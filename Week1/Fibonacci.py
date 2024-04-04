ce0c82a014878a1152598e24e60e3bd6d557bb84
Likhitha <likhitha.narayana@senecaglobal.com>
2024-04-04 17:45:58 +0530

    Fibonacci logic fix

--- Week1/Fibonacci.py
+++ Week1/Fibonacci.py
@@ -6,17 +6,14 @@
 def fibonacci_sequence(num):
     n1, n2 = 0, 1
     count = 0
-    if num == n1:
+    while count < num:
         print(n1)
-    elif num == n2:
-        print(n2)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
     else:
-        while count < num:
-            print(n1)
-            nth = n1 + n2
-            n1 = n2
-            n2 = nth
-            count += 1
+        print("Enter a valid number")
 
 
 number = int(input("Enter the value of n "))
